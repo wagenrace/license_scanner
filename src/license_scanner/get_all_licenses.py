6488e49b11e48e1c42bcb511490efb8b2ce0f63e
wagenrace <nijhof.dns@gmail.com>
2025-10-15 23:24:50 +0200

    get licenses with new functions

--- src/license_scanner/get_all_licenses.py
+++ src/license_scanner/get_all_licenses.py
@@ -1,69 +1,52 @@
 # %%
-from pkg_resources import working_set
+from typing import Dict, List
 
 from .parse_license import parse_license
 from .parse_license.licenses_synonyms import unknown_license
+from .get_all_packages import get_all_package_names
 
-import pkgutil
 import importlib_metadata as im
 
-for i in pkgutil.iter_modules():
+
+def get_license(name: str) -> List[str]:
+    all_licenses = []
     try:
-        if i.ispkg:
-            print(i.name)
-            str(im.metadata(i.name)).split("\n")
-    except:
-        continue
-
-
-def get_all_licenses():
-    all_licenses = {}
-    for key in working_set.normalized_to_canonical_keys:
-        """
-        The metadata of a package is stored in "metadata" or "PKG-INFO"
-        """
-        try:
-            package_name = working_set.normalized_to_canonical_keys[key]
-            package = working_set.by_key[package_name]
-            metadata_lines = package.get_metadata("METADATA").split("\n")
-        except:
-            try:
-                metadata_lines = package.get_metadata("PKG-INFO").split("\n")
-            except:
-                all_licenses[unknown_license] = all_licenses.get(
-                    unknown_license, []
-                ) + [key]
+        metas = im.metadata(name)
+    except im.PackageNotFoundError:
+        return [unknown_license]
+    for key, value in metas.items():
+        if key == "License-Expression":
+            license = value
+            if license:
+                all_licenses.append(parse_license(license))
+        if key == "License":
+            license = value
+            if license == "UNKNOWN":
+                # Sometimes this is unknown because it is not filled in
                 continue
+            if license:
+                all_licenses.append(parse_license(license))
+        if key == "Classifier":
+            if not value.startswith("License ::"):
+                continue
+            license = value.split("::")[-1]
+            if license.strip() == "OSI Approved":
+                continue
+            if license:
+                all_licenses.append(parse_license(license))
 
-        # You can get the license from license argument or classifier
-        license_arg_raw = None
-        license_classifier_raw = None
-        for line in metadata_lines:
-            line: str = line
-            if line.startswith("License-Expression: "):
-                license_arg_raw = line.replace("License-Expression: ", "")
-            if line.startswith("License: "):
-                license_arg_raw = line.replace("License: ", "")
-            if line.startswith("Classifier: License ::"):
-                license_classifier_raw = line.split(" :: ")[-1]
-                if license_classifier_raw.lower() in ["osi approved"]:
-                    license_classifier_raw = None
-
-        general_license = "NOT FOUND"
-        license_arg = parse_license(license_arg_raw)
-        license_classifier = parse_license(license_classifier_raw)
-
-        # You can get the license from license argument or classifier
-        if license_arg is not None and license_arg != unknown_license:
-            general_license = license_arg
-        elif license_classifier:
-            general_license = license_classifier
-        else:
-            general_license = unknown_license
-
-        all_licenses[general_license] = all_licenses.get(general_license, []) + [key]
-
+    all_licenses = list(set(all_licenses))
     return all_licenses
 
 
-# %%
+def get_all_licenses() -> Dict[str, str]:
+    all_licenses = {}
+    all_package_names = get_all_package_names()
+
+    for package_name in all_package_names:
+        licenses = get_license(package_name)
+
+        for license in licenses:
+            all_licenses[license] = all_licenses.get(license, []) + [package_name]
+
+    return all_licenses
