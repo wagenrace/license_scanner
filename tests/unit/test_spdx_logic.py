6e701e07030ed8f75b8db87fe2abd5a1a1269ecc
wagenrace <nijhof.dns@gmail.com>
2025-10-20 16:17:03 +0200

    add test for operation order

--- tests/unit/test_spdx_logic.py
+++ tests/unit/test_spdx_logic.py
@@ -30,12 +30,37 @@ def test_spdx_simple_expressions():
     allowed_licenses = ["MIT", "Apache-2.0", "BSD-3-Clause"]
 
     assert spdx_logic("MIT OR Apache-2.0", allowed_licenses) is True
+    assert spdx_logic("MIT OR GPL-3.0", allowed_licenses) is True
+    assert spdx_logic("BSD-4-Clause OR GPL-3.0", allowed_licenses) is False
     assert spdx_logic("MIT AND BSD-3-Clause", allowed_licenses) is True
     assert spdx_logic("MIT AND GPL-3.0", allowed_licenses) is False
+
+
+def test_spdx_multiple_operators_expressions():
+    # D.4.5 Order of precedence and parentheses
+    # AND should be resolved before OR
+
+    allowed_licenses = ["MIT", "Apache-2.0", "BSD-3-Clause"]
+
+    assert spdx_logic("MIT OR Apache-2.0 OR BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("MIT OR GPL-3.0 OR BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("GPL-2.0 OR GPL-3.0 OR LGPL-2.1", allowed_licenses) is False
+    assert spdx_logic("MIT AND Apache-2.0 AND BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("MIT AND Apache-2.0 AND GPL-3.0", allowed_licenses) is False
+    # AND is resolved before OR:
+    assert spdx_logic("MIT AND GPL-3.0 OR BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("GPL-2.0 AND GPL-3.0 OR BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("BSD-3-Clause OR GPL-2.0 AND GPL-3.0", allowed_licenses) is True
+    assert spdx_logic("GPL-2.0 OR GPL-3.0 AND BSD-3-Clause", allowed_licenses) is False
+    assert spdx_logic("GPL-3.0 AND BSD-3-Clause OR GPL-2.0", allowed_licenses) is False
 
 
 def test_spdx_single_level_brackets_expressions():
     allowed_licenses = ["MIT", "Apache-2.0", "BSD-3-Clause"]
 
     assert spdx_logic("(MIT OR Apache-2.0) AND BSD-3-Clause", allowed_licenses) is True
-    assert spdx_logic("(MIT OR GPL-3.0) AND BSD-3-Clause", allowed_licenses) is False
+    assert spdx_logic("BSD-3-Clause AND (MIT OR Apache-2.0)", allowed_licenses) is True
+    assert spdx_logic("(MIT OR GPL-3.0) AND BSD-3-Clause", allowed_licenses) is True
+    assert spdx_logic("BSD-3-Clause AND (MIT OR GPL-3.0)", allowed_licenses) is True
+    assert spdx_logic("(MIT OR BSD-3-Clause) AND GPL-3.0", allowed_licenses) is False
+    assert spdx_logic("GPL-3.0 AND (MIT OR BSD-3-Clause)", allowed_licenses) is False
