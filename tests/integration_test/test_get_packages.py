0cc47499223d8510b32fa4fca40a6bb5d6cad7bf
wagenrace <nijhof.dns@gmail.com>
2025-10-24 23:13:28 +0200

    add pillow to tests

--- tests/integration_test/test_get_packages.py
+++ tests/integration_test/test_get_packages.py
@@ -1,23 +1,11 @@
 from src.license_scanner.get_all_packages import get_all_package_names
+import pytest
 
 
-def test_get_all_package_names():
-    try:
-        import numpy  # noqa: F401
-        import absl  # noqa: F401
-        import skimage  # noqa: F401
-        import jaraco.text  # noqa: F401
-        import mergedeep  # noqa: F401
-    except ImportError:
-        raise ImportError(
-            "The requirements.txt for the integration tests must be installed"
-        )
-
+@pytest.mark.parametrize(
+    "model_name",
+    ["numpy", "mergedeep", "absl_py", "scikit_image", "jaraco.text", "pillow"],
+)
+def test_get_all_package_names(model_name):
     packages = get_all_package_names()
-    assert "numpy" in packages
-    assert "mergedeep" in packages
-    # Package name and import name differ
-    assert "absl_py" in packages
-    assert "scikit_image" in packages
-    # Package not found by setuptools at the time
-    assert "jaraco.text" in packages
+    assert model_name in packages
