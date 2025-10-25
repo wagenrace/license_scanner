9a5aa9bf14a691ce8c9e2a7b9f3463b9792ffab1
wagenrace <nijhof.dns@gmail.com>
2025-10-25 22:35:01 +0200

    add scikit-image to get_license test

--- tests/integration_test/test_get_license.py
+++ tests/integration_test/test_get_license.py
@@ -13,37 +13,21 @@ import re
 PYTHON_MINOR_VERSION = int(re.findall(r"3.(\d+).\d+", sys.version)[0])
 
 
-@pytest.mark.parametrize(
-    "model_name",
-    [
-        "mergedeep",
-        "jaraco.text",
-    ],
-)
+@pytest.mark.parametrize("model_name", ["mergedeep", "jaraco.text"])
 def test_get_licenses_mit(model_name):
     licenses = get_licenses(model_name)
     licenses = [parse_license(i) for i in licenses]
     assert mit_license in licenses
 
 
-@pytest.mark.parametrize(
-    "model_name",
-    [
-        "numpy",
-    ],
-)
-def test_get_licenses_bsd3(model_name):
+@pytest.mark.parametrize("model_name", ["numpy", "scikit_image"])
+def test_get_licenses_general_bsd(model_name):
     licenses = get_licenses(model_name)
     licenses = [parse_license(i) for i in licenses]
     assert bsd_license in licenses
 
 
-@pytest.mark.parametrize(
-    "model_name",
-    [
-        "absl_py",
-    ],
-)
+@pytest.mark.parametrize("model_name", ["absl_py"])
 def test_get_licenses_apache(model_name):
     licenses = get_licenses(model_name)
     licenses = [parse_license(i) for i in licenses]
@@ -55,6 +39,7 @@ def test_get_licenses_pillow():
     licenses = [parse_license(i) for i in licenses]
 
     # Pillow change there license in version 11
+    # Python 3.13 and up will have the new version of Pillow
     if PYTHON_MINOR_VERSION <= 12:
         assert historical_permission_notice in licenses
     else:
