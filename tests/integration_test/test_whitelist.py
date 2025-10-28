8d9a4a7dd186709b7c766d8f39795dc4308e6650
wagenrace <nijhof.dns@gmail.com>
2025-10-28 10:59:02 +0100

    add positive and negative integration tests

--- tests/integration_test/test_whitelist.py
+++ tests/integration_test/test_whitelist.py
@@ -1,7 +1,20 @@
 from unittest.mock import patch
 
 import pytest
-from src.license_scanner.cli import main
+from src.license_scanner.cli import main, Mode
+
+PYTEST_PACKAGES = [
+    "colorama",
+    "exceptiongroup",
+    "iniconfig",
+    "packaging",
+    "pluggy",
+    "pygments",
+    "pytest",
+    "tomli",
+    "typing_extensions",
+    "autocommand",
+]
 
 
 @pytest.mark.parametrize(
@@ -10,50 +23,51 @@ from src.license_scanner.cli import main
         (
             [
                 "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
                 "MiT",
                 "Apache-2.0",
+                "Apache license",
                 "Historical Permission Notice and Disclaimer (HPND)",
                 "CMU License (MIT-CMU)",
             ],
-            [],
+            [*PYTEST_PACKAGES],
         ),
         (
             [
                 "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
+                "MiT",
+                "Apache license",
                 "Apache-2.0",
-                "Historical Permission Notice and Disclaimer (HPND)",
-                "CMU License (MIT-CMU)",
             ],
-            ["mergedeep", "jaraco.text"],
+            ["pillow", *PYTEST_PACKAGES],
         ),
         (
             [
                 "BSD license",
-                "MiT",
-                "Apache-2.0",
-            ],
-            ["pillow"],
-        ),
-        (
-            [
-                "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
+                "Apache license",
                 "MiT",
                 "Historical Permission Notice and Disclaimer (HPND)",
                 "CMU License (MIT-CMU)",
             ],
-            ["absl_py"],
+            ["absl_py", *PYTEST_PACKAGES],
         ),
     ],
     ids=[
         "pure allowed licenses",
-        "no MIT",
         "no HPND or MIT-CMU",
         "no Apache 2.0",
     ],
 )
+@patch("src.license_scanner.cli.__get_arguments")
 @patch("src.license_scanner.cli.tomllib.load")
 def test_whitelist_integration_passing(
     toml_load_mock,
+    argparse_mock,
     allowed_licenses,
     allowed_packages,
 ):
@@ -65,4 +79,71 @@ def test_whitelist_integration_passing(
             }
         }
     }
+    argparse_mock.return_value = Mode.whitelist
     main()
+
+
+@pytest.mark.parametrize(
+    "allowed_licenses, allowed_packages",
+    [
+        (
+            [
+                "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
+                "Apache-2.0",
+                "Apache license",
+                "Historical Permission Notice and Disclaimer (HPND)",
+                "CMU License (MIT-CMU)",
+            ],
+            [*PYTEST_PACKAGES],
+        ),
+        (
+            [
+                "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
+                "MiT",
+                "Apache license",
+                "Apache-2.0",
+            ],
+            [*PYTEST_PACKAGES],
+        ),
+        (
+            [
+                "BSD license",
+                "BSD 2-clause license",
+                "BSD 3-clause license",
+                "Apache license",
+                "MiT",
+                "Historical Permission Notice and Disclaimer (HPND)",
+                "CMU License (MIT-CMU)",
+            ],
+            [*PYTEST_PACKAGES],
+        ),
+    ],
+    ids=[
+        "no MIT",
+        "no HPND or MIT-CMU",
+        "no Apache 2.0",
+    ],
+)
+@patch("src.license_scanner.cli.__get_arguments")
+@patch("src.license_scanner.cli.tomllib.load")
+def test_whitelist_integration_failing(
+    toml_load_mock,
+    argparse_mock,
+    allowed_licenses,
+    allowed_packages,
+):
+    toml_load_mock.return_value = {
+        "tool": {
+            "license_scanner": {
+                "allowed_licenses": allowed_licenses,
+                "allowed_packages": allowed_packages,
+            }
+        }
+    }
+    argparse_mock.return_value = Mode.whitelist
+    with pytest.raises(ValueError):
+        main()
